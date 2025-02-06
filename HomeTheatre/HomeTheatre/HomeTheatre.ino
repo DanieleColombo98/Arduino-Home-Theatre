7d72be00a12d0a3c3b86fb56f827dfc0e0c0b3d5
DanieleColombo98 <daniele.colombo13@studenti.unimi.it>
2025-02-06 17:43:08 +0100

    Aggiunto codice per gestire ventola

--- HomeTheatre/HomeTheatre/HomeTheatre.ino
+++ HomeTheatre/HomeTheatre/HomeTheatre.ino
@@ -1,7 +1,11 @@
-#include <DHT.h>      //Librerira sensore DHT
+#include <DHT.h>      // Libreria sensore DHT
 
 #define DHTPIN 2      // Pin DHT11
 #define DHTTYPE DHT11
+
+#define FAN_PWM 5     // Pin per velocità della ventola
+#define FAN_DIR1 4    // Pin direzione 1
+#define FAN_DIR2 3    // Pin direzione 2
 
 DHT dht(DHTPIN, DHTTYPE);  // Inizializza DHT
 
@@ -9,12 +13,22 @@ void setup() {
     Serial.begin(9600);
     Serial.println("Inizializzazione DHT11");
     dht.begin();  // Avvia DHT
+
+    // Configurazione pin della ventola
+    pinMode(FAN_PWM, OUTPUT);
+    pinMode(FAN_DIR1, OUTPUT);
+    pinMode(FAN_DIR2, OUTPUT);
+    
+    // Spegne la ventola all'avvio
+    digitalWrite(FAN_DIR1, LOW);
+    digitalWrite(FAN_DIR2, LOW);
+    analogWrite(FAN_PWM, 0);
 }
 
 void loop() {
     delay(2000);
 
-    //Lettura umidità e temperatura
+    // Lettura umidità e temperatura
     float humidity = dht.readHumidity();
     float temperature = dht.readTemperature(); 
 
@@ -30,5 +44,17 @@ void loop() {
     Serial.print("Temperatura: ");
     Serial.print(temperature);
     Serial.println(" °C");
-}
 
+    // Controllo della ventola
+    if (temperature > 24 || humidity > 50) {
+        Serial.println("Ventola ACCESA");
+        digitalWrite(FAN_DIR1, HIGH);
+        digitalWrite(FAN_DIR2, LOW);
+        analogWrite(FAN_PWM, 255);
+    } else {
+        Serial.println("Ventola SPENTA");
+        digitalWrite(FAN_DIR1, LOW);
+        digitalWrite(FAN_DIR2, LOW);
+        analogWrite(FAN_PWM, 0);
+    }
+}
